dc99cb0831d3f40abede8e041152607284ce8eb8
Matt Levy <milevy1@gmail.com>
2019-04-05 10:29:15 -0600

    Removed comments

--- spec/features/user_can_see_all_snacks_for_a_vending_machine_spec.rb
+++ spec/features/user_can_see_all_snacks_for_a_vending_machine_spec.rb
@@ -16,10 +16,3 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     expect(page).to have_content("Price: $#{oreos.price}")
   end
 end
-
-# User Story 1 of 3
-#
-# As a user
-# When I visit a vending machine show page
-# I see the name of all of the snacks associated
-# with that vending machine along with their price
